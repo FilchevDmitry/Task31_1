3c8f676138a92766b9704001e8f48d2f4a9d1ef9
Dmitry <filchev_dmitry@mail.ru>
2023-03-28 20:30:04 +0300

    '123'

--- Task31_1/Task31_1.cpp
+++ Task31_1/Task31_1.cpp
@@ -1,1 +1,63 @@
-﻿
+﻿#include <iostream>
+
+class Toy
+{
+private:
+	std::string name;
+public:
+	Toy(std::string _name) :name(_name) {};
+	Toy() :Toy("SomeToy") {};
+};
+
+class shared_ptr_toy
+{
+private:
+	Toy* obj;
+	int count = 0;
+	void remove()
+	{
+		--(count);
+		if (count == 0)
+		{
+			delete obj;
+			count = 0;
+		}
+	}
+public:
+	shared_ptr_toy(Toy* toy)
+	{
+		obj = toy;
+		count=1;
+	};
+	shared_ptr_toy(const shared_ptr_toy* other)
+	{
+		obj = other->obj;
+		count = other->count;
+		++(count);
+	}
+	shared_ptr_toy& operator=(const shared_ptr_toy* other)
+	{
+		if (count > 0)
+			remove();
+		if (this != other)
+		{
+			obj = other->obj;
+			count = other->count;
+			++(count);
+		}
+		return *this;
+	}
+	~shared_ptr_toy()
+	{
+		remove();
+	};
+};
+
+
+int main()
+{
+	Toy s("dfg");
+	shared_ptr_toy b(s);
+
+	return 0;
+}
