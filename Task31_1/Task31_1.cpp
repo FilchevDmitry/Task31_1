e440af37cd126334df6e3276a0e6ce4b73a2cfa4
DmityFilchev <filchev_dmitry@mail.ru>
2023-03-28 16:40:25 +0300

    '28'

--- Task31_1/Task31_1.cpp
+++ Task31_1/Task31_1.cpp
@@ -1,5 +1,5 @@
-﻿#include <iostream>
-
+﻿#include<iostream>
+#include<string>
 class Toy
 {
 private:
@@ -24,10 +24,15 @@ private:
 		}
 	}
 public:
+	shared_ptr_toy()
+	{
+		obj = new Toy();
+		count = new int(1);
+	}
 	shared_ptr_toy(Toy* toy)
 	{
 		obj = toy;
-		*count = 1;
+		count = new int(1);
 	};
 	shared_ptr_toy(const shared_ptr_toy* other)
 	{
@@ -50,14 +55,46 @@ public:
 	~shared_ptr_toy() 
 	{ 
 		remove(); 
-	};
+	}
+};
+shared_ptr_toy make_shared_toy(Toy* toy)
+{
+	return shared_ptr_toy(toy);
+};
+shared_ptr_toy make_shared_toy(std::string& _name)
+{
+	Toy d(_name);
+	shared_ptr_toy f(d);
+	return f;
 };
 
+class Dog
+{
+private:
+	std::string name;
+	int age;
+	shared_ptr_toy lovelyToy;
+public:
+	Dog(std::string _name, shared_ptr_toy(toy), int _age) :name(_name), lovelyToy(toy)
+	{
+		if (_age >= 0 && _age < 20)
+			age = _age;
+
+	}
+	Dog(std::string _name) :Dog(_name, make_shared_toy("SomeToy"), 0) {};
+	Dog(int _age) : Dog("None", make_shared_toy("SomeToy"), _age) {};
+	Dog() :Dog("None", make_shared_toy("SomeToy"), 0) {};
+	void copyToy(const Dog& oth)
+	{
+		lovelyToy = oth.lovelyToy;
+	}
+};
 
 int main()
 {
-	Toy s("dfg");
-	
+	Dog s("dfg");
+	Dog f();
+	Dog x(s);
 		
 	return 0;
 }
